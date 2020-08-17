a5f38b85223db8d185d9d17365f410f4ddae4597
studiomfb <57592762+studioMFB@users.noreply.github.com>
2020-08-17 18:15:48 +0100

    Add files via upload

--- php/phpContactForm.php
+++ php/phpContactForm.php
@@ -1,12 +1,40 @@
-<?php $firstName = $_POST['firstName'];
-$lastName = $_POST['lastName'];
-$email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "studiomfb@gmail.com";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+<?php
+
+//if (!empty($_POST)) {
+if (isset($_POST['submit'])) {
+$nameUser = $_POST['name'];
+$emailUser = $_POST['email'];
+$messageUser = $_POST['message'];
+/*
+if (empty($name)) {
+    $errors[] = 'Name is empty';
+}
+if (empty($email)) {
+    $errors[] = 'Email is empty';
+} else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $errors[] = 'Email is invalid';
+}
+if (empty($message)) {
+    $errors[] = 'Message is empty';
+}
+if (!empty($errors)) {
+   $allErrors = join('<br/>', $errors);
+   $errorMessage = "<p style='color: red;'>{$allErrors}</p>";
+}
+*/
+$mailTo = "studiomfb@gmail.com";
+$subject = "contact form - Gallery Unique";
+$headers = "From: ".$emailUser;
+$txt = "You have recieve an email from " .$nameUser.".\n\n".$messageUser;
+
+if (mail($mailTo, $txt, $headers)){
+    echo "<h1>Sent Successfuly! Thank you"." ".$nameUser.", We will contact you shortly.</h1>";
+}
+else{
+    echo "OOPS! Something went wromg!";
+}
+
+header("Location: index.php?mailsend");
+}
+
 ?>
